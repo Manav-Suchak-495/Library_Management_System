448457e43934aebaf1a482a2f62f9ed0fe4f825a
Manav Suchak <manavsuchak21@gmail.com>
2026-07-08 02:37:14 +0530

    fixing verify-session

--- LMS/src/ProtectedRoute.tsx
+++ LMS/src/ProtectedRoute.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import axios from 'axios';
 
-const ProtectedRoute = async () => {
+const ProtectedRoute = () => {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  await useEffect(() => {
+  useEffect( () => {
     const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
         axios.get(`${apiUrl}/verify-session`).then((response) =>{
         setIsAuthenticated(true);
