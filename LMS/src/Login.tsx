45e6ef9926fab976d7f472f9d5f02a2e6cddeb90
Manav Suchak <manavsuchak21@gmail.com>
2026-07-08 04:01:56 +0530

    fixing T

--- LMS/src/Login.tsx
+++ LMS/src/Login.tsx
@@ -40,7 +40,7 @@ function Login() {
             const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000";
             await axios.post(`${apiUrl}/Login`, {"email": email, "password": password}).then((response) =>{
             console.log('Login successful');
-            sessionStorage.setItem("Token", response.data.token);
+            sessionStorage.setItem("Token", response.data.Token);
             navigate('/home');
         }).catch ((error)=>{
             console.error('Login failed or network error:', error);
